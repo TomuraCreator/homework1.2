aa9e4fa81a71b3bea855691935526fcdead015be
tery <vladislavutkin92@gmail.com>
2019-02-28 16:05:47 +0300

    изменен метод сортировки массива

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,12 @@
 'use strict';
 
 let val = [74989, 74990, 84990, 62000, 58480, 61800];
-let i = -1;
 function bestScores() {
     let bs = Math.max.apply(null, val);
     return console.log(`Самый лучший балл - ${bs}.`);
     }
 function midScores(arr) {
-    for (let i = 0, endI = arr.length - 1; i < endI; i++) {
-        for (let j = 0, endJ = endI - i; j < endJ; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let swap = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = swap;
-            }
-        }
-    }
+    arr.sort();
     let mid = (arr[3] + arr[4] + arr[5]) / 3;
     return console.log(`Средний балл топ-3 игроков = ${mid}`);
 }
